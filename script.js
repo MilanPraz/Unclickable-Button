20773658282b92a06b89ce3edeb39bdd4a7bca2c
Milanpraz <mln.praz12@gmail.com>
2023-06-04 22:14:48 +0545

    first version

---
+++ script.js
@@ -0,0 +1,43 @@
+let wrapper=document.querySelector("#wrapper")
+let question=document.querySelector("#question")
+let btnGroup=document.querySelector("#btn-group")
+let yes=document.querySelector("#yes")
+let no=document.querySelector("#no")
+
+
+const wrapperRect=wrapper.getBoundingClientRect();
+const nobtnRect=no.getBoundingClientRect();
+
+yes.addEventListener("click",()=>{
+    question.style.opacity=0.1
+
+    setTimeout(()=>{
+            question.style.opacity=1
+            question.style.transition="0.4s"
+
+    },500)
+    question.innerHTML='Thank You!!!';
+    console.log("heloo")
+
+})
+
+no.addEventListener("mouseenter",()=>{
+
+const randomWidth= Math.floor(Math.random()*(wrapperRect.width -nobtnRect.width));
+const randomHeight= Math.floor(Math.random()*(wrapperRect.height -nobtnRect.height));
+
+console.log(randomWidth)
+console.log(randomHeight)
+no.style.opacity=0.3;
+no.style.scale=0.5;
+
+setTimeout(()=>{
+    no.style.opacity=1;
+    no.style.scale=1;
+
+    no.style.transition="0.3s"
+
+    no.style.right=`${randomWidth}px`
+    no.style.top=`${randomHeight}px`
+},100)
+})
