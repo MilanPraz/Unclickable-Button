535757f1dcc560efc9d0d399ec010aeca625af04
Milanpraz <mln.praz12@gmail.com>
2024-02-14 16:37:24 +0545

    motu

--- script.js
+++ script.js
@@ -1,63 +1,68 @@
-let wrapper=document.querySelector("#wrapper")
-let question=document.querySelector("#question")
-let btnGroup=document.querySelector("#btn-group")
-let yes=document.querySelector("#yes")
-let no=document.querySelector("#no")
+let wrapper = document.querySelector("#wrapper");
+let question = document.querySelector("#question");
+let btnGroup = document.querySelector("#btn-group");
+let yes = document.querySelector("#yes");
+let no = document.querySelector("#no");
 
+const wrapperRect = wrapper.getBoundingClientRect();
+const nobtnRect = no.getBoundingClientRect();
 
-const wrapperRect=wrapper.getBoundingClientRect();
-const nobtnRect=no.getBoundingClientRect();
+yes.addEventListener("click", () => {
+  question.style.opacity = 0.1;
 
-yes.addEventListener("click",()=>{
-    question.style.opacity=0.1
+  setTimeout(() => {
+    question.style.opacity = 1;
+    question.style.transition = "0.4s";
+  }, 500);
+  question.innerHTML = "Thank You Motu, You're So Sweet";
+  console.log("heloo");
+});
 
-    setTimeout(()=>{
-            question.style.opacity=1
-            question.style.transition="0.4s"
+no.addEventListener("mouseenter", () => {
+  console.log("btn ko width", nobtnRect.width);
+  console.log("wrapper ko width", wrapperRect.width);
+  const randomWidth = Math.floor(
+    Math.random() * (wrapperRect.width - nobtnRect.width)
+  );
+  const randomHeight = Math.floor(
+    Math.random() * (wrapperRect.height - nobtnRect.height)
+  );
 
-    },500)
-    question.innerHTML="Thank You, You're So Sweet";
-    console.log("heloo")
+  console.log(randomWidth);
+  console.log(randomHeight);
+  no.style.opacity = 0.3;
+  no.style.scale = 0.5;
 
-})
+  setTimeout(() => {
+    no.style.opacity = 1;
+    no.style.scale = 1;
 
-no.addEventListener("mouseenter",()=>{
+    no.style.transition = "0.3s";
 
-const randomWidth= Math.floor(Math.random()*(wrapperRect.width -nobtnRect.width));
-const randomHeight= Math.floor(Math.random()*(wrapperRect.height -nobtnRect.height));
+    no.style.right = `${randomWidth}px`;
+    no.style.top = `${randomHeight}px`;
+  }, 100);
+});
+no.addEventListener("click", () => {
+  const randomWidth = Math.floor(
+    Math.random() * (wrapperRect.width - nobtnRect.width)
+  );
+  const randomHeight = Math.floor(
+    Math.random() * (wrapperRect.height - nobtnRect.height)
+  );
 
-console.log(randomWidth)
-console.log(randomHeight)
-no.style.opacity=0.3;
-no.style.scale=0.5;
+  console.log(randomWidth);
+  console.log(randomHeight);
+  no.style.opacity = 0.3;
+  no.style.scale = 0.5;
 
-setTimeout(()=>{
-    no.style.opacity=1;
-    no.style.scale=1;
+  setTimeout(() => {
+    no.style.opacity = 1;
+    no.style.scale = 1;
 
-    no.style.transition="0.3s"
+    no.style.transition = "0.3s";
 
-    no.style.right=`${randomWidth}px`
-    no.style.top=`${randomHeight}px`
-},100)
-})
-no.addEventListener("click",()=>{
-
-const randomWidth= Math.floor(Math.random()*(wrapperRect.width -nobtnRect.width));
-const randomHeight= Math.floor(Math.random()*(wrapperRect.height -nobtnRect.height));
-
-console.log(randomWidth)
-console.log(randomHeight)
-no.style.opacity=0.3;
-no.style.scale=0.5;
-
-setTimeout(()=>{
-    no.style.opacity=1;
-    no.style.scale=1;
-
-    no.style.transition="0.3s"
-
-    no.style.right=`${randomWidth}px`
-    no.style.top=`${randomHeight}px`
-},100)
-})
+    no.style.right = `${randomWidth}px`;
+    no.style.top = `${randomHeight}px`;
+  }, 100);
+});
